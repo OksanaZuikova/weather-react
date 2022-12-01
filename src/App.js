8615c75af9eb61ac310ab9db05c7609c9dccb969
Oksana Zuikova <Oksy82714@gmail.com>
2022-12-01 12:37:13 +0200

    Added API integration

--- src/App.js
+++ src/App.js
@@ -4,7 +4,22 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <Weather />
+      <div className="container">
+        <Weather
+          defaultCity="
+Kyiv"
+        />
+        <p className="git">
+          <a
+            href="/https://github.com/OksanaZuikova/weather-react"
+            target="_blank"
+            className="git-link"
+          >
+            Open-sourse code
+          </a>{" "}
+          by Oksana Zuikova
+        </p>
+      </div>
     </div>
   );
 }
